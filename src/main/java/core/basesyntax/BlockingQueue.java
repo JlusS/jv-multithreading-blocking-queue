016e9afbde7be50715b3e75f65e2dee5662b24e8
Egor <llikojia162@gmail.com>
2025-10-17 12:50:01 +0200

    done

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -12,16 +12,31 @@ public class BlockingQueue<T> {
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        while (queue.size() >= capacity) {
+            System.out.println("Queue is full, waiting to put element: " + element);
+            wait();
+        }
+        queue.add(element);
+        System.out.println("Added element: " + element);
+        notify();
     }
 
     public synchronized T take() throws InterruptedException {
         // write your code here
-        return null;
+        while (queue.isEmpty()) {
+            System.out.println("Queue is empty, waiting to take element");
+            wait();
+        }
+        T element = queue.poll();
+        System.out.println("Took element: " + element);
+        notify();
+        return element;
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
+        while(!queue.isEmpty()) {
+            return false;
+        }
         return true;
     }
 }
